3994057c25a8837814988104c2551099ff30394a
Yumin Chu <yuminc03@gmail.com>
2023-12-03 23:04:48 +0900

    Initial Commit

---
+++ SwiftfulThinking/SwiftfulThinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingApp.swift
+//  SwiftfulThinking
+//
+//  Created by Yumin Chu on 2023/12/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
