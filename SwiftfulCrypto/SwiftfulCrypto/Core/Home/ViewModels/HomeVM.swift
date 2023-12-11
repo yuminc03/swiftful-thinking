7cfc31d99cda1331acbeeddbdc8034093c2c3ce6
Yumin Chu <yuminc03@gmail.com>
2023-12-11 20:31:22 +0900

    Feat: CoinDataService - combine을 사용해서 coin Data 불러오기, VM에서 저장하기

--- SwiftfulCrypto/SwiftfulCrypto/Core/Home/ViewModels/HomeVM.swift
+++ SwiftfulCrypto/SwiftfulCrypto/Core/Home/ViewModels/HomeVM.swift
@@ -6,14 +6,24 @@
 //
 
 import Foundation
+import Combine
 
 class HomeVM: ObservableObject {
   @Published var allCoins = [CoinModel]()
   @Published var portfolioCoins = [CoinModel]()
   
+  private let dataService = CoinDataService()
+  private var cancelBag = Set<AnyCancellable>()
+  
   init() {
-    DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-      self.allCoins.append(DeveloperPreview.shared.coin)
-    }
+    addSubscriber()
+  }
+  
+  func addSubscriber() {
+    dataService.$allCoins
+      .sink { [weak self] returnedCoins in
+        self?.allCoins = returnedCoins
+      }
+      .store(in: &cancelBag)
   }
 }
