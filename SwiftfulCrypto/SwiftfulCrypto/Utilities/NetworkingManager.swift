a283edfe4d3e38746417178bbf0758aebd3977f7
Yumin Chu <yuminc03@gmail.com>
2023-12-12 22:08:41 +0900

    Feat: NetworkingManager - CoinDataService에서 사용하는 코드 조각을 더 작게 분리하기

---
+++ SwiftfulCrypto/SwiftfulCrypto/Utilities/NetworkingManager.swift
@@ -0,0 +1,46 @@
+import Foundation
+import Combine
+
+final class NetworkingManager {
+  enum NetworkingError: LocalizedError {
+    case badURLResponse(url: URL)
+    case unknown
+    var errorDescription: String? {
+      switch self {
+      case let .badURLResponse(url):
+        return "[🔥] Bad response from URL. \(url)"
+        
+      case .unknown:
+        return "[⚠️] Unknown error occured."
+      }
+    }
+  }
+  
+  static func download(url: URL) -> AnyPublisher<Data, Error> {
+    return URLSession.shared.dataTaskPublisher(for: url)
+      .subscribe(on: DispatchQueue.global(qos: .default))
+      .tryMap { try handleURLResponse(output: $0, url: url) }
+      .receive(on: DispatchQueue.main)
+      .eraseToAnyPublisher()
+  }
+  
+  static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+    guard let response = output.response as? HTTPURLResponse,
+          response.statusCode >= 200 && response.statusCode < 300
+    else {
+      throw NetworkingError.badURLResponse(url: url)
+    }
+    
+    return output.data
+  }
+  
+  static func handleCompletion(completion: Subscribers.Completion<Error>) {
+    switch completion {
+    case .finished:
+      break
+      
+    case let .failure(error):
+      print(error.localizedDescription)
+    }
+  }
+}
