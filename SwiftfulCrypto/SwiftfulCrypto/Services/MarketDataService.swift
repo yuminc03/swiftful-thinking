1d14b000c0d94984f30a70113e106990bf9fe5c9
Yumin Chu <yuminc03@gmail.com>
2024-01-04 22:25:12 +0900

    Feat: 코인마켓 데이터 Model, API 요청 메서드

---
+++ SwiftfulCrypto/SwiftfulCrypto/Services/MarketDataService.swift
@@ -0,0 +1,29 @@
+//
+//  MarketDataService.swift
+//  SwiftfulCrypto
+//
+//  Created by Yumin Chu on 1/4/24.
+//
+
+import Foundation
+import Combine
+
+final class MarketDataService {
+  @Published var marketData: MarketDataModel?
+  private var marketDataSubsciption: AnyCancellable?
+  
+  init() {
+    getData()
+  }
+  
+  private func getData() {
+    guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else { return }
+    
+    marketDataSubsciption = NetworkingManager.download(url: url)
+      .decode(type: GlobalData.self, decoder: JSONDecoder())
+      .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] globalData in
+        self?.marketData = globalData.data
+        self?.marketDataSubsciption?.cancel()
+      })
+  }
+}
