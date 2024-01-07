18b125e4d008daf7b3299d10088d2afa5ec47f47
Yumin Chu <yuminc03@gmail.com>
2024-01-07 21:36:03 +0900

    Feat: PortfolioView - Coin을 클릭하면 price, amount를 입력할 수 있는 TextField 나타내기

--- SwiftfulCrypto/SwiftfulCrypto/Core/Home/Views/PortfolioView.swift
+++ SwiftfulCrypto/SwiftfulCrypto/Core/Home/Views/PortfolioView.swift
@@ -10,35 +10,37 @@ import SwiftUI
 struct PortfolioView: View {
   @EnvironmentObject private var vm: HomeVM
   @State private var selectedCoin: CoinModel?
+  @State private var quantityText = ""
   
   var body: some View {
     NavigationView {
       ScrollView {
         VStack(alignment: .leading, spacing: 0) {
           SearchBarView(searchText: $vm.searchText)
-          ScrollView(.horizontal, showsIndicators: false) {
-            LazyHStack(spacing: 10) {
-              ForEach(vm.allCoins) { coin in
-                CoinLogoView(coin: coin)
-                  .frame(width: 75)
-                  .padding(4)
-                  .onTapGesture {
-                    withAnimation(.easeIn) {
-                      selectedCoin = coin
-                    }
-                  }
-                  .background(
-                    RoundedRectangle(cornerRadius: 10)
-                      .stroke(
-                        selectedCoin?.id == coin.id ?
-                        Color.theme.green : Color.clear,
-                        lineWidth: 1
-                      )
-                  )
+          coinLogoList
+          if selectedCoin != nil {
+            VStack(spacing: 20) {
+              HStack {
+                Text("Current price of \(selectedCoin?.symbol.uppercased() ?? ""):")
+                Spacer()
+                Text(selectedCoin?.currentPrice.asCurrencyWith6Decimals() ?? "")
+              }
+              Divider()
+              HStack {
+                Text("Amount is your portfolio:")
+                Spacer()
+                TextField("Ex. 1.4", text: $quantityText)
+                  .multilineTextAlignment(.trailing)
+                  .keyboardType(.decimalPad)
+              }
+              Divider()
+              HStack {
+                Text("Current value:")
+                Spacer()
+                Text("")
               }
             }
-            .padding(.vertical, 4)
-            .padding(.leading)
+            .foregroundColor(.theme.accent)
           }
         }
       }
@@ -52,9 +54,38 @@ struct PortfolioView: View {
   }
 }
 
-struct ProtfolioView_Previews: PreviewProvider {
+struct PortfolioView_Previews: PreviewProvider {
   static var previews: some View {
     PortfolioView()
       .environmentObject(dev.homeVM)
+  }
+}
+
+extension PortfolioView {
+  private var coinLogoList: some View {
+    ScrollView(.horizontal, showsIndicators: false) {
+      LazyHStack(spacing: 10) {
+        ForEach(vm.allCoins) { coin in
+          CoinLogoView(coin: coin)
+            .frame(width: 75)
+            .padding(4)
+            .onTapGesture {
+              withAnimation(.easeIn) {
+                selectedCoin = coin
+              }
+            }
+            .background(
+              RoundedRectangle(cornerRadius: 10)
+                .stroke(
+                  selectedCoin?.id == coin.id ?
+                  Color.theme.green : Color.clear,
+                  lineWidth: 1
+                )
+            )
+        }
+      }
+      .padding(.vertical, 4)
+      .padding(.leading)
+    }
   }
 }
