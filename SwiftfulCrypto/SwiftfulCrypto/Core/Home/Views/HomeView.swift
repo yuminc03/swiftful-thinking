7de8ec163393380c2a5670833b48d9afef6c3444
Yumin Chu <yuminc03@gmail.com>
2024-01-21 00:58:11 +0900

    Fix: HomeView backgroundColor 적용안된 부분 수정

--- SwiftfulCrypto/SwiftfulCrypto/Core/Home/Views/HomeView.swift
+++ SwiftfulCrypto/SwiftfulCrypto/Core/Home/Views/HomeView.swift
@@ -115,6 +115,7 @@ extension HomeView {
           .onTapGesture {
             segue(coin: coin)
           }
+          .listRowBackground(Color.theme.background)
       }
     }
     .listStyle(.plain)
