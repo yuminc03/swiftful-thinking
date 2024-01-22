2d24dfe7f3c3e6bcd1609c8eec781dace09042e2
Yumin Chu <yuminc03@gmail.com>
2024-01-22 21:08:37 +0900

    Feat: 프로젝트 생성

---
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingAdvancedLearningApp.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by Yumin Chu on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
