7a3104764b841d32d59b363f3b2d1d34b409981e
Yumin Chu <yuminc03@gmail.com>
2024-02-03 22:20:23 +0900

    Feat: ViewBuilder 사용 예시

--- SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/ViewBuilderBootcamp.swift
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/ViewBuilderBootcamp.swift
@@ -10,20 +10,44 @@ import SwiftUI
 struct ViewBuilderBootcamp: View {
   var body: some View {
     VStack {
-      HeaderViewRegular(title: "New Title", description: "Hello")
-      HeaderViewRegular(title: "Another Title", description: nil)
+      HeaderViewRegular(
+        title: "New Title",
+        description: "Hello",
+        iconName: "heart.fill"
+      )
+      HeaderViewRegular(
+        title: "Another Title",
+        description: nil,
+        iconName: nil
+      )
+      HeaderViewGeneric(title: "Generic Title") {
+        Text("Hi")
+      }
+      HeaderViewGeneric(title: "Generic Title") {
+        VStack {
+          Text("Hello")
+          Image(systemName: "heart.fill")
+          Text("Hello")
+        }
+      }
+      CustomHStack {
+        Text("Coffee")
+        Text("Coffee")
+      }
+      
       Spacer()
     }
   }
 }
 
 #Preview {
-  ViewBuilderBootcamp()
+  LocalViewBuilder(type: .one)
 }
 
 struct HeaderViewRegular: View {
   let title: String
   let description: String?
+  let iconName: String?
   
   var body: some View {
     VStack(alignment: .leading) {
@@ -34,10 +58,77 @@ struct HeaderViewRegular: View {
         Text(description)
           .font(.callout)
       }
+      
+      if let iconName {
+        Image(systemName: iconName)
+      }
       RoundedRectangle(cornerRadius: 2)
         .frame(height: 2)
     }
     .frame(maxWidth: .infinity, alignment: .leading)
     .padding()
+  }
+}
+
+struct HeaderViewGeneric<Content: View>: View {
+  let title: String
+  let content: Content
+  
+  init(title: String, @ViewBuilder content: () -> Content) {
+    self.title = title
+    self.content = content()
+  }
+  
+  var body: some View {
+    VStack(alignment: .leading) {
+      Text(title)
+        .font(.largeTitle)
+        .fontWeight(.semibold)
+      
+      content
+      
+      RoundedRectangle(cornerRadius: 2)
+        .frame(height: 2)
+    }
+    .frame(maxWidth: .infinity, alignment: .leading)
+    .padding()
+  }
+}
+
+struct CustomHStack<Content: View>: View {
+  let content: Content
+  
+  init(@ViewBuilder content: () -> Content) {
+    self.content = content()
+  }
+  
+  var body: some View {
+    HStack {
+      content
+    }
+  }
+}
+
+struct LocalViewBuilder: View {
+  enum ViewType {
+    case one, two, three
+  }
+  let type: ViewType
+  
+  var body: some View {
+    if type == .one {
+      Text("One!")
+    } else if type == .two {
+      VStack {
+        Text("TODO")
+        Image(systemName: "heart.fill")
+      }
+    } else if type == .three {
+      VStack {
+        Text("Three!")
+        Image(systemName: "heart.fill")
+      }
+    }
+                
   }
 }
