f4fcb6ad225cae1fdd0903c1e1f65611d22e3ac9
Yumin Chu <yuminc03@gmail.com>
2024-02-06 22:28:27 +0900

    Feat: CustomTabView 만들기

---
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/CustomTabBar/CustomTabBarView.swift
@@ -0,0 +1,69 @@
+//
+//  CustomTabBarView.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by Yumin Chu on 2/6/24.
+//
+
+import SwiftUI
+
+struct CustomTabBarView: View {
+  let tabs: [TabBarItem]
+  @State private var selection = TabBarItem(iconName: "house", title: "Home", color: .red)
+  
+  var body: some View {
+    HStack {
+      ForEach(tabs, id: \.self) { tab in
+        tabView(tab: tab)
+          .onTapGesture {
+            switchToTab(tab: tab)
+          }
+      }
+    }
+    .padding(6)
+    .background(Color.white.ignoresSafeArea(edges: .bottom))
+  }
+}
+
+struct CustomTabBarView_Previews: PreviewProvider {
+  static let tabs: [TabBarItem] = [
+    .init(iconName: "house", title: "Home", color: .red),
+    .init(iconName: "heart", title: "Favorites", color: .blue),
+    .init(iconName: "person", title: "Profile", color: .green)
+  ]
+  
+  static var previews: some View {
+    VStack {
+      Spacer()
+      CustomTabBarView(tabs: tabs)
+    }
+  }
+}
+
+extension CustomTabBarView {
+  private func tabView(tab: TabBarItem) -> some View {
+    VStack {
+      Image(systemName: tab.iconName)
+        .font(.subheadline)
+      Text(tab.title)
+        .font(.system(size: 10, weight: .semibold, design: .rounded))
+    }
+    .foregroundColor(selection == tab ? tab.color : .gray)
+    .padding(.vertical, 8)
+    .frame(maxWidth: .infinity)
+    .background(selection == tab ? tab.color.opacity(0.2) : .clear)
+    .cornerRadius(10)
+  }
+  
+  private func switchToTab(tab: TabBarItem) {
+    withAnimation(.easeInOut) {
+      selection = tab
+    }
+  }
+}
+
+struct TabBarItem: Hashable {
+  let iconName: String
+  let title: String
+  let color: Color
+}
