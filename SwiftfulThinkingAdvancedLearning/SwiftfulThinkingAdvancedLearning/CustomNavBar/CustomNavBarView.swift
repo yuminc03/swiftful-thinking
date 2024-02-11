f9e2175e11244c5e9a46132a63a189706bc5a866
Yumin Chu <yuminc03@gmail.com>
2024-02-11 21:38:59 +0900

    Feat: CustomNavBarView - 커스텀 NavigationBar 따로 만들기

---
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/CustomNavBar/CustomNavBarView.swift
@@ -0,0 +1,65 @@
+//
+//  CustomNavBarView.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by Yumin Chu on 2/11/24.
+//
+
+import SwiftUI
+
+struct CustomNavBarView: View {
+  @State private var showBackButton = true
+  @State private var title = "Title"
+  @State private var subTitle: String? = "SubTitle"
+  
+  var body: some View {
+    HStack {
+      if showBackButton {
+        backButton
+      }
+      Spacer()
+      titleSection
+      Spacer()
+      if showBackButton {
+        backButton
+          .opacity(0)
+      }
+    }
+    .padding()
+    .tint(.white)
+    .foregroundColor(.white)
+    .font(.headline)
+    .background(
+      Color.blue
+        .ignoresSafeArea(edges: .top)
+    )
+  }
+}
+
+#Preview {
+  VStack {
+    CustomNavBarView()
+    Spacer()
+  }
+}
+
+extension CustomNavBarView {
+  private var backButton: some View {
+    Button {
+      
+    } label: {
+      Image(systemName: "chevron.left")
+    }
+  }
+  
+  private var titleSection: some View {
+    VStack(spacing: 4) {
+      Text(title)
+        .font(.title)
+        .fontWeight(.semibold)
+      if let subTitle {
+        Text(subTitle)
+      }
+    }
+  }
+}
