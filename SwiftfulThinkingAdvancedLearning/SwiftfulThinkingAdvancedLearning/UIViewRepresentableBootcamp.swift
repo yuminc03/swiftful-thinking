375726585606ccbea1b5a301117c3153666dd6d9
Yumin Chu <yuminc03@gmail.com>
2024-02-13 22:58:13 +0900

    Feat: UIViewRepresentableBootcamp 새 파일 생성

---
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/UIViewRepresentableBootcamp.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewRepresentableBootcamp.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by Yumin Chu on 2/13/24.
+//
+
+import SwiftUI
+
+// Convert a UIView form UIKit to SwiftUI
+struct UIViewRepresentableBootcamp: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+#Preview {
+  UIViewRepresentableBootcamp()
+}
