63c911589587044edc70ffbac0d0e63bc84dbca6
Yumin Chu <yuminc03@gmail.com>
2024-02-14 21:02:53 +0900

    Feat: UIViewRepresentable로 UIView, UITextField 사용하기

--- SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/UIViewRepresentableBootcamp.swift
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/UIViewRepresentableBootcamp.swift
@@ -9,11 +9,53 @@ import SwiftUI
 
 // Convert a UIView form UIKit to SwiftUI
 struct UIViewRepresentableBootcamp: View {
+  @State private var text = ""
+  
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    VStack {
+      Text("Hello, World!")
+      
+      TextField("Type here...", text: $text)
+        .frame(height: 55)
+        .background(Color.gray)
+      
+      UITextFieldViewReporesentable()
+        .frame(height: 55)
+        .background(Color.gray)
+    }
   }
 }
 
 #Preview {
   UIViewRepresentableBootcamp()
+}
+
+struct UITextFieldViewReporesentable: UIViewRepresentable {
+  func makeUIView(context: Context) -> some UIView {
+    return getTextField()
+  }
+  
+  func updateUIView(_ uiView: UIViewType, context: Context) {
+    
+  }
+  
+  private func getTextField() -> UITextField {
+    let v = UITextField(frame: .zero)
+    let placeholder = NSAttributedString(
+      string: "Type here...",
+      attributes: [.foregroundColor: UIColor.green]
+    )
+    v.attributedPlaceholder = placeholder
+    return v
+  }
+}
+
+struct BasicUIViewRepresentable: UIViewRepresentable {
+  func makeUIView(context: Context) -> some UIView {
+    let v = UIView()
+    v.backgroundColor = .blue
+    return v
+  }
+  
+  func updateUIView(_ uiView: UIViewType, context: Context) { }
 }
