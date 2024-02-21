a05440f77186874b617e861f1346cf8a8b326b62
Yumin Chu <yuminc03@gmail.com>
2024-02-21 22:15:11 +0900

    Refactor: protocol 적용

--- SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/DependencyInjectionBootcamp.swift
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/DependencyInjectionBootcamp.swift
@@ -20,7 +20,11 @@ struct PostsModel: Identifiable, Codable {
   let body: String
 }
 
-final class ProductionDataService {
+protocol DataServiceProtocol {
+  func getData() -> AnyPublisher<[PostsModel], Error>
+}
+
+final class ProductionDataService: DataServiceProtocol {
   let url: URL
   
   init(url: URL) {
@@ -36,12 +40,30 @@ final class ProductionDataService {
   }
 }
 
+// Test 목적
+final class MockDataService: DataServiceProtocol {
+  private let testData: [PostsModel]
+  
+  init(data: [PostsModel]?) {
+    self.testData = data ?? [
+      .init(userId: 1, id: 1, title: "One", body: "one"),
+      .init(userId: 2, id: 2, title: "Two", body: "two")
+    ]
+  }
+  
+  func getData() -> AnyPublisher<[PostsModel], Error> {
+    Just(testData)
+      .tryMap { $0 }
+      .eraseToAnyPublisher()
+  }
+}
+
 final class DependencyInjectionVM: ObservableObject {
   @Published var dataArray = [PostsModel]()
   private var cancelBag = Set<AnyCancellable>()
-  let dataService: ProductionDataService
+  let dataService: DataServiceProtocol
   
-  init(dataService: ProductionDataService) {
+  init(dataService: DataServiceProtocol) {
     self.dataService = dataService
     loadPosts()
   }
@@ -60,7 +82,7 @@ final class DependencyInjectionVM: ObservableObject {
 struct DependencyInjectionBootcamp: View {
   @StateObject private var vm: DependencyInjectionVM
   
-  init(dataService: ProductionDataService) {
+  init(dataService: DataServiceProtocol) {
     self._vm = StateObject(wrappedValue: .init(dataService: dataService))
   }
   
@@ -76,9 +98,13 @@ struct DependencyInjectionBootcamp: View {
 }
 
 struct DependencyInjectionBootcamp_Previews: PreviewProvider {
-  static let dataService = ProductionDataService(
-    url: URL(string: "https://jsonplaceholder.typicode.com/posts")!
-  )
+//  static let dataService = ProductionDataService(
+//    url: URL(string: "https://jsonplaceholder.typicode.com/posts")!
+//  )
+//  static let dataService = MockDataService(data: nil)
+  static let dataService = MockDataService(data: [
+    .init(userId: 1234, id: 1234, title: "test", body: "test")
+  ])
   
   static var previews: some View {
     DependencyInjectionBootcamp(dataService: dataService)
